0defb540cc6a584072afe593fadd119f84f6d9cf
manish <aryam7842@gmail.com>
2025-01-23 14:19:14 +0530

    add method printName in Solution.java

--- ChaiaurDjango/Solution.java
+++ ChaiaurDjango/Solution.java
@@ -1,6 +1,10 @@
 
 public class Solution {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void printName(String name) {
+        System.out.println("Your name is: " + name);
     }
+}
